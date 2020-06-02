6e52e87ce44953a2199ff6caf6741915260b4308
nbloomf <nathan.bloomfield@automattic.com>
2020-06-02 17:00:32 -0500

    Use item_original_subtotal_* properties from endpoint response

--- client/my-sites/checkout/composite-checkout/wpcom/components/wp-order-review-line-items.js
+++ client/my-sites/checkout/composite-checkout/wpcom/components/wp-order-review-line-items.js
@@ -174,14 +174,11 @@ WPLineItem.propTypes = {
 };
 
 function LineItemPrice( { item } ) {
-	const volume = item.wpcom_meta?.volume || 1;
-	const amountBeforeDiscounts = volume * item.wpcom_meta?.item_original_cost_integer;
 	return (
 		<LineItemPriceUI>
-			{ item.amount.value < amountBeforeDiscounts ? (
+			{ item.amount.value < item.wpcom_meta?.item_original_subtotal_integer ? (
 				<>
-					// Problem: if volume > 1 the display cost here is incorrect.
-					<s>{ item.wpcom_meta?.item_original_cost_display }</s> { item.amount.displayValue }
+					<s>{ item.wpcom_meta?.item_original_subtotal_display }</s> { item.amount.displayValue }
 				</>
 			) : (
 				renderDisplayValueMarkdown( item.amount.displayValue )
